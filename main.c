0165bc111443f813752621df423005a8f9670bce
Travis Musall <116574308+travisPhase2@users.noreply.github.com>
2023-06-16 14:21:31 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,10 @@
-int main(void) {
-    printf("I love dogs and cats!");
-    return 0;
+#include <stdio.h>
+#include <stdint.h>
+
+int main(void)
+{
+    uint64_t dx = 0x77E435B08;
+    while (dx) {
+        putchar(0x726F6C6564574820 >> (((dx >>= 3) & 7) << 3) & 0xFF);
+    }
 }
